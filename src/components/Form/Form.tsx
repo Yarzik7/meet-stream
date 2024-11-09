c933cd658630c879be370379e81fb85cc4308804
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-11-09 21:51:07 +0200

    Add button to form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import CustomButton from '../CustomButton/CustomButton';
 
 const Form = () => {
-  return <form>Form</form>;
+  return (
+    <form>
+      <CustomButton>Login</CustomButton>
+    </form>
+  );
 };
 
 export default Form;
