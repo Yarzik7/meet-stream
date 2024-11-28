36e511951e0f801ef3aaa56e356b66d79a67a773
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-11-28 11:55:19 +0200

    Work with LoginForm

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -4,11 +4,32 @@ import Form from '../Form/Form';
 import Input from '../Input/Input';
 
 const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target;
+    switch (name) {
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        return alert('Type is broken');
+    }
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+
+    console.log({ email, password });
+  };
+
   return (
     <>
-      <Form buttonCaption="Login">
-        <Input label="Email" name="email" />
-        <Input label="Password" name="password" />
+      <Form buttonCaption="Login" onSubmit={handleSubmit}>
+        <Input label="Email" name="email" onChange={handleChange} />
+        <Input label="Password" name="password" onChange={handleChange} />
       </Form>
     </>
   );
